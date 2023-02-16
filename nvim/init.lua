993f9a7609fa8a8747fddcf5781a06da7fffdc83
Eric Moncada <emoncada@vmware.com>
2023-02-16 13:39:37 -0700

    latest(config): files

--- nvim/init.lua
+++ nvim/init.lua
@@ -189,35 +189,10 @@ local efmls_config = {
     end
 }
 local luals_config = {
-    ls = 'sumneko_lua',
+    ls = 'lua_ls',
     get_config = function(on_attach)
-        local system_name
-        if vim.fn.has('mac') == 1 then
-            system_name = 'macOS'
-        elseif vim.fn.has('unix') == 1 then
-            system_name = 'Linux'
-        elseif vim.fn.has('win32') == 1 then
-            system_name = 'Windows'
-        else
-            print('Unsupported system for sumneko')
-        end
-        local sumneko_root_path = os.getenv('HOME') .. '/src/lua-language-server'
-        local sumneko_binary = sumneko_root_path .. '/bin/' .. system_name .. '/lua-language-server'
-        local cmd = {sumneko_binary, '-E', sumneko_root_path .. '/main.lua'}
-        local runtime_path = vim.split(package.path, ';')
-        table.insert(runtime_path, 'lua/?.lua')
-        table.insert(runtime_path, 'lua/?/init.lua')
-        return {
-            cmd = cmd,
-            on_attach = on_attach,
-            settings = {
-                Lua = {
-                    runtime = {version = 'LuaJIT', path = runtime_path},
-                    diagnostics = {globals = {'vim'}},
-                    workspace = {library = vim.api.nvim_get_runtime_file('', true), maxPreload = 2000, preloadFileSize = 1000}
-                }
-            }
-        }
+        local cmd = {'lua-language-server'}
+        return {cmd = cmd, on_attach = on_attach, settings = {Lua = {telemetry = {enable = false}}}}
     end
 }
 local pyls_config = {
