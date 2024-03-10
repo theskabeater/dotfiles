4c57f9d888f59a29eb3eff2c29396fb5f18d6410
Eric Moncada <theskabeater@gmail.com>
2024-03-09 23:07:10 -0700

    latest(config): files

--- nvim/lua/plugins/configs/cmp.lua
+++ nvim/lua/plugins/configs/cmp.lua
@@ -47,7 +47,7 @@ return {
 	mapping = {
 		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<Esc>"] = cmp.mapping.abort(),
@@ -76,6 +76,7 @@ return {
 	},
 
 	sources = {
+		{ name = "nvim_lsp_signature_help" },
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -76,6 +76,7 @@ local plugins = {
 			},
 			{
 				"saadparwaiz1/cmp_luasnip",
+				"hrsh7th/cmp-nvim-lsp-signature-help",
 				"hrsh7th/cmp-nvim-lua",
 				"hrsh7th/cmp-nvim-lsp",
 				"hrsh7th/cmp-cmdline",
